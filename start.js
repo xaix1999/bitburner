6351c618e6d446d35fb6b599012632d6018a0c54
xaix1999 <88251983+xaix1999@users.noreply.github.com>
2022-02-11 12:52:02 -0700

    Update start.js

--- start.js
+++ start.js
@@ -1,29 +1,23 @@
 /** @param {NS} ns **/
+const baseUrl = "https://raw.githubusercontent.com/xaix1999/bitburner/main/"
+const filesToDownload = [
+	"hnet.js",
+	"less8.js",
+	"pserv.js",
+	"share.js",
+	"shortsticky.js",
+	"sinewave.js"
+]
+
 export async function main(ns) {
- if (ns.getHostname() !== "home") {
- throw new Exception("Run the script from home");
- }
-//
- ns.kill("less8.js", "home")
- ns.kill("pserv.js", "home")
- ns.kill("hnet.js", "home")
- ns.kill("shortsticky.js", "home")
- ns.kill("sinewave.js", "home")
- ns.rm("less8.js", "home")
- ns.rm("pserv.js", "home")
- ns.rm("hnet.js", "home")
- ns.rm("shortsticky.js", "home")
- ns.rm("sinewave.js", "home")
-//
- await ns.wget("https://raw.githubusercontent.com/xaix1999/bitburner/main/less8.js", "less8.js");
- await ns.wget("https://raw.githubusercontent.com/xaix1999/bitburner/main/pserv.js", "pserv.js");
- await ns.wget("https://raw.githubusercontent.com/xaix1999/bitburner/main/hnet.js", "hnet.js");
- await ns.wget("https://raw.githubusercontent.com/xaix1999/bitburner/main/shortsticky.js", "shortsticky.js");
- await ns.wget("https://raw.githubusercontent.com/xaix1999/bitburner/main/sinewave.js", "sinewave.js");
-//
- ns.run("less8.js", 1);
- ns.run("pserv.js", 1);
- //ns.run("hnet.js", 1);
- //ns.run("shortsticky.js", 1);
- //ns.run("sinewave.js", 1);
- }
+	for (let i = 0; i < filesToDownload.length; i++) {
+		const filename = filesToDownload[i]
+		const path = baseUrl + filename
+		await ns.scriptKill(filename, 'home')
+		await ns.rm(filename)
+		await ns.sleep(100)
+		await ns.wget(path, filename)
+	}
+
+	await ns.exec("less8.js");
+}
