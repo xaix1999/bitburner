f11be8b328455f66d7ab0a5d8655508a39071205
xaix1999 <88251983+xaix1999@users.noreply.github.com>
2022-02-20 17:11:24 -0700

    Update less8.js

--- less8.js
+++ less8.js
@@ -2,7 +2,7 @@
 export async function main(ns) {
 	var temp = false;
 	var scanAdd = false;
-	//var hTarget = [
+	//var hTarget = []
 	var hTarget = ["n00dles", "CSEC", "global-pharm", "foodnstuff", "sigma-cosmetics", "joesguns", "hong-fang-tea", "harakiri-sushi", "iron-gym", "zer0", "nectar-net", "max-hardware", "neo-net", "phantasy", "omega-net", "silver-helix", "the-hub", "comptek", "johnson-ortho", "crush-fitness", "avmnite-02h", "netlink", "catalyst", "I.I.I.I", "rothman-uni", "zb-institute", "summit-uni", "syscore", "aevum-police", "alpha-ent", "lexo-corp", "rho-construction", "millenium-fitness", "galactic-cyber", "aerocorp", "snap-fitness", "omnia", "unitalife", "deltaone", "defcomm", "zeus-med", "icarus", "solaris", "univ-energy", "zb-def", "nova-med", "infocomm", "taiyang-digital", "titan-labs", "microdyne", "applied-energetics", "run4theh111z", "stormtech", "fulcrumtech", "helios", "vitalife", ".", "omnitek", "kuai-gong", "4sigma", "nwo", "clarkinc", "b-and-a", "blade", "powerhouse-fitness", "ecorp", "megacorp", "fulcrumassets"]
 	var hostList;
 	var targetList;
@@ -57,10 +57,10 @@ export async function main(ns) {
 	async function sortServers() {
 		targetList = []; hostList = [[ns.getServerMaxRam("home"), "home"]];
 		for (let i = 0; i <= hTarget.length - 1; i++) {
-			let sTarget = hTarget[i]; var serverGrowth = ns.getServerGrowth(sTarget) * -1 + 100;
+			let sTarget = hTarget[i];
 			if (ns.serverExists(sTarget) && ns.hasRootAccess(sTarget)) {
 				if (ns.getServerMaxMoney(sTarget) > 0 || ns.getServerMaxRam(sTarget) > 2) {
-					temp = [Math.trunc(ns.getServerMaxMoney(sTarget) * 0.01 / ns.getServerMinSecurityLevel(sTarget) / serverGrowth / ns.getServerRequiredHackingLevel(sTarget)), sTarget]
+					temp = [Math.trunc(ns.getServerMaxMoney(sTarget) * 0.1 / ns.getServerMinSecurityLevel(sTarget) / (ns.getServerGrowth(sTarget) * -1 + 100) / ns.getServerRequiredHackingLevel(sTarget)), sTarget]
 					if (ns.getServerMaxMoney(sTarget) != 0 && !targetList.includes(temp) && ns.getServerRequiredHackingLevel(sTarget) <= ns.getHackingLevel()) {
 						targetList.push(temp); targetList = arraySort(targetList)
 					}
@@ -95,46 +95,40 @@ export async function main(ns) {
 			if (ns.serverExists(hostList[i][1])) {
 				//
 				await ns.scp(files, "home", hostList[i][1]);
-				var secNum = (ns.getServerSecurityLevel(targetList[x][1]) - (ns.getServerMinSecurityLevel(targetList[x][1]) + 5));
-				var caSh = (Math.trunc(ns.getServerMoneyAvailable(targetList[x][1])));
-				var mCash = (Math.trunc(ns.getServerMaxMoney(targetList[x][1]) * 0.70));
+				var secNum = Math.trunc(ns.getServerSecurityLevel(targetList[x][1]) - (ns.getServerMinSecurityLevel(targetList[x][1]) + 5));
+				var caSh = Math.trunc(ns.getServerMoneyAvailable(targetList[x][1]));
+				var mCash = Math.trunc(ns.getServerMaxMoney(targetList[x][1]) * 0.70);
 				var gThreads = 0; var wThreads = 0; var hThreads = 0;
 				if (hostList[i][1] == "home") { freeRam = 0; } else if (ns.serverExists(hostList[i][1])) { freeRam = ns.getServerMaxRam(hostList[i][1]) - ns.getServerUsedRam(hostList[i][1]) }
 				//
-				if (Math.trunc(secNum) < 1) {
-					if (Math.trunc(mCash / caSh) <= 1) {
-						//var maxThreads = Math.trunc((0.40 / ns.hackAnalyze(targetList[x][1]))); // / hostList.length);
-						var ramThreads = Math.max(Math.trunc(freeRam / 1.70), 0)
-						//var hThreads = Math.min(ramThreads, maxThreads)
-						var hThreads = Math.trunc(ramThreads / (hostList.length - 1))
-						if (hThreads < 1) { } else {
-							if (await ns.exec(files[2], hostList[i][1], hThreads, targetList[x][1])) {
-								//ns.print("[" + Date().substr(16, 8) + "] " + files[2] + "-" + hostList[i][1] + "-h" + hThreads + "-" + targetList[x][1]);
-							}
+				if (secNum < 1 && mCash / caSh <= 1) {
+					//var maxThreads = Math.ceil(0.70 / ns.hackAnalyze(targetList[x][1]) / (hostList.length - 1));
+					var ramThreads = Math.max(Math.trunc(freeRam / 1.70), 0);
+					//var hThreads = Math.min(ramThreads, maxThreads);
+					var hThreads = Math.trunc(ramThreads);
+					if (hThreads < 1) { } else {
+						if (await ns.exec(files[2], hostList[i][1], hThreads, targetList[x][1])) {
+							//ns.print("[" + Date().substr(16, 8) + "] " + files[2] + "-" + hostList[i][1] + "-h" + hThreads + "-" + targetList[x][1]);
 						}
 					}
 				}
-				if (Math.trunc(secNum) > 0) {
-					var maxThreads = Math.ceil((ns.getServerSecurityLevel(targetList[x][1]) - ns.getServerMinSecurityLevel(targetList[x][1]) + 5) / ns.weakenAnalyze(1, 1)); // / hostList.length);
+				if (secNum > 0 && ns.getWeakenTime(targetList[x][1]) < 600000) {
+					var maxThreads = Math.ceil((ns.getServerSecurityLevel(targetList[x][1]) - ns.getServerMinSecurityLevel(targetList[x][1]) + 5) / ns.weakenAnalyze(1, 1) / (hostList.length - 1));
 					var ramThreads = Math.max(Math.trunc(freeRam / 1.75), 0);
 					var wThreads = Math.min(ramThreads, maxThreads);
 					if (wThreads < 1) { } else {
-						if (ns.getWeakenTime(targetList[x][1]) < 300000) {
-							if (await ns.exec(files[0], hostList[i][1], wThreads, targetList[x][1])) {
-								//ns.print("[" + Date().substr(16, 8) + "] " + files[0] + "-" + hostList[i][1] + "-w" + wThreads + "-" + targetList[x][1]);
-							}
+						if (await ns.exec(files[0], hostList[i][1], wThreads, targetList[x][1])) {
+							//ns.print("[" + Date().substr(16, 8) + "] " + files[0] + "-" + hostList[i][1] + "-w" + wThreads + "-" + targetList[x][1]);
 						}
 					}
 				}
-				if (Math.trunc(mCash / caSh) > 1) {
-					var maxThreads = Math.ceil(ns.growthAnalyze(targetList[x][1], ns.getServerMaxMoney(targetList[x][1]) * 0.70 / Math.max(ns.getServerMoneyAvailable(targetList[x][1]), 1), 1)); // / hostList.length);
+				if (mCash / caSh > 1 && ns.getGrowTime(targetList[x][1]) < 600000) {
+					var maxThreads = Math.ceil(ns.growthAnalyze(targetList[x][1], ns.getServerMaxMoney(targetList[x][1]) * 0.70 / Math.max(ns.getServerMoneyAvailable(targetList[x][1]), 1), 1) / (hostList.length - 1));
 					var ramThreads = Math.max(Math.floor(freeRam / 1.75), 0);
 					var gThreads = Math.min(ramThreads, maxThreads);
 					if (gThreads < 1) { } else {
-						if (ns.getGrowTime(targetList[x][1]) < 300000) {
-							if (await ns.exec(files[1], hostList[i][1], gThreads, targetList[x][1])) {
-								//ns.print("[" + Date().substr(16, 8) + "] " + files[1] + "-" + hostList[i][1] + "-g" + gThreads + "-" + targetList[x][1]);
-							}
+						if (await ns.exec(files[1], hostList[i][1], gThreads, targetList[x][1])) {
+							//ns.print("[" + Date().substr(16, 8) + "] " + files[1] + "-" + hostList[i][1] + "-g" + gThreads + "-" + targetList[x][1]);
 						}
 					}
 				}
@@ -155,11 +149,15 @@ export async function main(ns) {
 	}
 	//
 	ns.tail("less8.js");
+	var danceFrame = ["/", "-", "\\", "|"]; var u = 0;
 	while (true) {
 		await scanExes();
 		await sortServers();
 		await theBusiness();
 		await ns.clearLog();
+		//await ns.print(targetList);
+		if (u > danceFrame.length - 1) { u = 0 }
+		await ns.print(danceFrame[u]); u++
 		await ns.print("less8.js income per/second - $" + format(ns.getScriptIncome("less8.js", "home")));
 		await ns.sleep(1000);
 	}
