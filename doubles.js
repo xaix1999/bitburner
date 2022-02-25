cf7b8543b4b9157745894d72afd3a991a8990303
xaix1999 <88251983+xaix1999@users.noreply.github.com>
2022-02-25 15:36:35 -0700

    Update doubles.js

--- doubles.js
+++ doubles.js
@@ -1,5 +1,8 @@
 /** @param {NS} ns **/
 export async function main(ns) {
+	ns.clearLog();
+	ns.disableLog("disableLog");
+	ns.disableLog("sleep");
 	async function sleeveDo() {
 		var sleeveNum = ns.sleeve.getNumSleeves();
 		for (let x = 0; x <= sleeveNum - 1; x++) {
@@ -9,21 +12,41 @@ export async function main(ns) {
 			if (ns.sleeve.getSleeveStats(x).shock == 0 && ns.sleeve.getSleeveStats(x).sync < 100) {
 				if (await ns.sleeve.setToSynchronize(x)) { await ns.print("Sleeve #" + x + " is doing sync.") }
 			}
-			if (ns.sleeve.getSleeveStats(x).shock == 0 && ns.sleeve.getSleeveStats(x).sync == 100) {
-				await ns.sleeve.setToGymWorkout(0, "Powerhouse Gym", "Train Strength");
-				await ns.sleeve.setToGymWorkout(1, "Powerhouse Gym", "Train Strength");
-				await ns.sleeve.setToGymWorkout(2, "Powerhouse Gym", "Train Defense");
-				await ns.sleeve.setToGymWorkout(3, "Powerhouse Gym", "Train Defense");
-				await ns.sleeve.setToGymWorkout(4, "Powerhouse Gym", "Train Dexterity");
-				await ns.sleeve.setToGymWorkout(5, "Powerhouse Gym", "Train Dexterity");
-				await ns.sleeve.setToGymWorkout(6, "Powerhouse Gym", "Train Agility");
-				await ns.sleeve.setToGymWorkout(7, "Powerhouse Gym", "Train Agility");
-				await ns.exit();
+			if (ns.sleeve.getSleeveStats(x).shock == 0 && ns.sleeve.getSleeveStats(x).sync == 100 && ns.sleeve.getSleeveStats(x).strength < 201) {
+				await ns.sleeve.travel(x, "Sector-12");
+				await ns.sleeve.setToGymWorkout(x, "Powerhouse Gym", "Train Strength");
+			}
+			if (ns.sleeve.getSleeveStats(x).strength >= 201 && ns.sleeve.getSleeveStats(x).defense < 201){
+				await ns.sleeve.travel(x, "Sector-12");
+				await ns.sleeve.setToGymWorkout(x, "Powerhouse Gym", "Train Defense");
+			}
+			if (ns.sleeve.getSleeveStats(x).defense >= 201 && ns.sleeve.getSleeveStats(x).dexterity < 201){
+				await ns.sleeve.travel(x, "Sector-12");
+				await ns.sleeve.setToGymWorkout(x, "Powerhouse Gym", "Train Dexterity");
+			}
+			if (ns.sleeve.getSleeveStats(x).dexterity >= 201 && ns.sleeve.getSleeveStats(x).agility < 201){
+				await ns.sleeve.travel(x, "Sector-12");
+				await ns.sleeve.setToGymWorkout(x, "Powerhouse Gym", "Train Agility");
+			}
+			if (ns.sleeve.getSleeveStats(x).agility >= 201 && ns.sleeve.getSleeveStats(x).hacking < 201) {
+				await ns.sleeve.travel(x, "Volhaven");
+				//await ns.sleeve.setToUniversityCourse(x, "ZB Institute of Technology", "Leadership");
+				await ns.sleeve.setToUniversityCourse(x, "ZB Institute of Technology", "Algorithms");
+			}
+			if (ns.sleeve.getSleeveStats(x).hacking >= 201 && ns.sleeve.getSleeveStats(x).strength >= 201 && ns.sleeve.getSleeveStats(x).defense >= 201 && ns.sleeve.getSleeveStats(x).dexterity >= 201 && ns.sleeve.getSleeveStats(x).agility >= 201 && !(ns.sleeve.getTask(x).crime == "Larceny")) {
+				await ns.sleeve.setToCommitCrime(x, "Larceny")
+
 			}
 		}
 	}
 	while (true) {
 		await sleeveDo();
-		await ns.sleep(1000);
+		await ns.sleep(30000);
+		//await ns.exit();
+
+		//await ns.sleeve.setToCommitCrime(x,"Mug")
+		//await ns.sleeve.setToCommitCrime(x,"Deal Drugs")
+		//await ns.sleeve.setToCommitCrime(x,"Homicide")
+		//
 	}
 }
