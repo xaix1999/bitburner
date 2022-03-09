6153c3a9f221d2a11128fd2f2896063a0ff618ab
xaix1999 <88251983+xaix1999@users.noreply.github.com>
2022-03-08 17:13:46 -0700

    Update 8plus.js

--- 8plus.js
+++ 8plus.js
@@ -2,7 +2,6 @@
 export async function main(ns) {
 	var temp = false;
 	var scanAdd = false;
-	var hTarget9 = ["megacorp", "ecorp", "kuai-gong", "b-and-a", "nwo", "4sigma", "omnitek", "clarkinc", "blade"]
 	var hTarget = ["n00dles", "foodnstuff", "sigma-cosmetics", "joesguns", "hong-fang-tea", "harakiri-sushi", "iron-gym", "zer0", "nectar-net", "CSEC", "max-hardware", "neo-net", "phantasy", "omega-net", "silver-helix", "the-hub", "comptek", "johnson-ortho", "crush-fitness", "avmnite-02h", "netlink", "catalyst", "I.I.I.I", "rothman-uni", "zb-institute", "summit-uni", "syscore", "aevum-police", "alpha-ent", "lexo-corp", "rho-construction", "millenium-fitness", "galactic-cyber", "aerocorp", "global-pharm", "snap-fitness", "omnia", "unitalife", "deltaone", "defcomm", "zeus-med", "icarus", "solaris", "univ-energy", "zb-def", "nova-med", "infocomm", "taiyang-digital", "titan-labs", "microdyne", "applied-energetics", "run4theh111z", "stormtech", "fulcrumtech", "helios", "vitalife", ".", "omnitek", "kuai-gong", "4sigma", "nwo", "clarkinc", "b-and-a", "blade", "powerhouse-fitness", "ecorp", "megacorp", "fulcrumassets"]
 	var hostList;
 	var targetList;
@@ -36,8 +35,7 @@ export async function main(ns) {
 	function sortServers() {
 		targetList = []; hostList = [];
 		for (let i = 0; i <= hTarget.length - 1; i++) {
-			if (ns.getHackingLevel() >= ns.getServerRequiredHackingLevel("ecorp")) { var sTarget = hTarget9[i]; }
-			else if (ns.getHackingLevel() < ns.getServerRequiredHackingLevel("ecorp")) { var sTarget = hTarget[i]; }
+			let sTarget = hTarget[i];
 			if (ns.serverExists(sTarget) && ns.hasRootAccess(sTarget)) {
 				if (ns.getServerMaxMoney(sTarget) > 0 || ns.getServerMaxRam(sTarget) > 2) {
 					temp = [Math.trunc(ns.getServerMaxMoney(sTarget) * 0.1 / ns.getServerMinSecurityLevel(sTarget)), sTarget]
@@ -53,7 +51,7 @@ export async function main(ns) {
 		}
 		var scanAdd = ns.scan("home")
 		for (let i = 0; i <= scanAdd.length - 1; i++) {
-			let sTarget = scanAdd[i]
+			let sTarget = scanAdd[i];
 			if (scanAdd[i].startsWith("s-") || scanAdd[i].startsWith("hacknet-node-")) {
 				if (ns.serverExists(sTarget) && ns.hasRootAccess(sTarget)) {
 					temp = [ns.getServerMaxRam(sTarget), sTarget]
@@ -68,7 +66,6 @@ export async function main(ns) {
 	function stuTH() {
 		var targetString = [];
 		var tLS = targetList.length
-		if (tLS > 9) { tLS = 9 }
 		for (let b = 0; b < tLS; b++) {
 			if (ns.serverExists(targetList[b][1])) {
 				temp = [targetList[b][1]];
@@ -90,13 +87,15 @@ export async function main(ns) {
 	async function theBusiness() {
 		var x = 0; var i = 0; var hostList = sessionStorage.hostList.split(","); var targetList = sessionStorage.targetList.split(",");
 		for (let y = 1; y <= hostList.length * targetList.length; y++) {
-			await ns.sleep(1);
+			await ns.sleep(10);
 			//
 			if (x > targetList.length - 1) { x = 0; i++ }
 			if (i > hostList.length - 1) { i = hostList.length - 1; y = hostList.length * targetList.length + 1 }
 			if (ns.serverExists(hostList[i])) {
 				//
-				await ns.scp(files, "home", hostList[i]);
+				if (!ns.fileExists(files[0])) { await ns.scp(files[0], "home", hostList[i]); }
+				if (!ns.fileExists(files[1])) { await ns.scp(files[1], "home", hostList[i]); }
+				if (!ns.fileExists(files[2])) { await ns.scp(files[2], "home", hostList[i]); }
 				//
 				ns.exec("8pW.js", "home", 1, hostList[i], targetList[x]);
 			} x++
@@ -108,6 +107,6 @@ export async function main(ns) {
 		sortServers();
 		stuTH();
 		await theBusiness();
-		await ns.sleep(1000);
+		//await ns.sleep(1000);
 	}
 }
