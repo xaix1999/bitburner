5f86242489227d24e9aa4326a4402ff080d90b7d
xaix1999 <88251983+xaix1999@users.noreply.github.com>
2022-03-31 00:31:06 -0700

    Create sCp.js

---
+++ sCp.js
@@ -0,0 +1,20 @@
+/** @param {NS} ns **/
+async function scp(ns) {
+	var files = ["weak.js", "grow.js", "hack.js", "cryptInit.js"];
+	var hostListsS = sessionStorage.hostList.split(",");
+	for (let i = 0; i <= hostListsS.length - 1; i++) {
+		if (!ns.fileExists(files[0], hostListsS[i])) {
+			await ns.scp(files[0], "home", hostListsS[i]);
+		}
+		if (!ns.fileExists(files[1], hostListsS[i])) {
+			await ns.scp(files[1], "home", hostListsS[i]);
+		}
+		if (!ns.fileExists(files[2], hostListsS[i])) {
+			await ns.scp(files[2], "home", hostListsS[i]);
+		}
+		if (!ns.fileExists(files[3], hostListsS[i])) {
+			await ns.scp(files[3], "home", hostListsS[i]);
+		}
+	}
+}
+export { scp };
