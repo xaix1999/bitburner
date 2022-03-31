0576c4b1ba72d1c45435b975cc2f8574837e77bf
xaix1999 <88251983+xaix1999@users.noreply.github.com>
2022-03-31 00:19:42 -0700

    Update 8pW.js

--- 8pW.js
+++ 8pW.js
@@ -1,45 +1,89 @@
-export async function main(ns) {
+/** @param {NS} ns **/
+import { crypt, decrypt } from "/cryptInit.js";
+//
+async function magicHack(ns, cHost, cTarget) {
+	//
+	const testText = "Success!"
+	// encrypting
+	var encrypted_text = crypt(sessionStorage.cryptKey, testText);
+	// decrypting
+	var decrypted_string = decrypt(sessionStorage.cryptKey, encrypted_text);
+	// test crypt
+	while (decrypted_string !== testText) {
+		await ns.sleep(10);
+		var encrypted_text = crypt(sessionStorage.cryptKey, testText);
+		var decrypted_string = decrypt(sessionStorage.cryptKey, encrypted_text);
+	}
+	//
+	function funcRoute(ns, method, ...args) {
+		const call = () => eval("ns." + method)(...args);
+		try {
+			return call();
+		} catch {
+			return call();
+		}
+	}
+	const _ns = (...args) => funcRoute(ns, ...args);
+	//
 	var files = ["weak.js", "grow.js", "hack.js"];
-	var hostList = ns.args[0]; var targetList = ns.args[1];
+	//var cHost = ns.args[0].toString(); var cTarget = ns.args[1].toString();
+	var hostList = decrypt(sessionStorage.cryptKey, cHost);
+	var targetList = decrypt(sessionStorage.cryptKey, cTarget);
+	//var hostList = ns.args[0]; var targetList = ns.args[1];
+	var ddtargetList = crypt(sessionStorage.cryptKey, targetList)
 	var hostListsS = sessionStorage.hostList.split(",");
 	//
-	var freeRam = ns.getServerMaxRam(hostList) - ns.getServerUsedRam(hostList);
+	var ramMax = _ns("getServerMaxRam", hostList);
+	var ramUsed = _ns("getServerUsedRam", hostList);
+	var secWeek = _ns("weakenAnalyze", 1, 1);
+	var secHa = _ns("hackAnalyze", targetList);
+	var moneyMax = _ns("getServerMaxMoney", targetList);
+	var moneyAvail = _ns("getServerMoneyAvailable", targetList) + 1;
+	var maxAvail = moneyMax / moneyAvail;
+	var groA = Math.max(_ns("growthAnalyze", targetList, Math.ceil(maxAvail), 1), 1);
+	var secCurrent = _ns("getServerSecurityLevel", targetList);
+	var secMin = _ns("getServerMinSecurityLevel", targetList);
+	var freeRam = (ramMax - ramUsed);
+	var weakTime = _ns("getWeakenTime", targetList);
+	var growTime = _ns("getGrowTime", targetList);
+	var hackTime = _ns("getHackTime", targetList);
 	//
-	var secNum = Math.trunc(ns.getServerSecurityLevel(targetList) - (ns.getServerMinSecurityLevel(targetList) + 5));
-	var caSh = Math.trunc(ns.getServerMoneyAvailable(targetList));
-	var mCash = Math.trunc(ns.getServerMaxMoney(targetList) * 0.70);
+	var secNum = Math.trunc(secCurrent - (secMin + 5));
+	var caSh = Math.trunc(moneyAvail);
+	var mCash = Math.trunc(moneyMax * 0.70);
 	var wRam = 1.75; var gRam = 1.75; var hRam = 1.70;
 	//
 	if (secNum < 1 && mCash / caSh <= 1 && freeRam >= 5.20) {
 		//
-		var ghUpKeep = Math.min(Math.ceil(ns.growthAnalyze(targetList, Math.ceil(ns.getServerMaxMoney(targetList) / ns.getServerMoneyAvailable(targetList)), 1) + 1), 12)
-		var wSleep = ns.getWeakenTime(targetList);
-		var gSleep = ns.getGrowTime(targetList);
-		var hsleep = ns.getHackTime(targetList);
+		var ghUpKeep = Math.min(groA, 12)
+		var wSleep = weakTime;
+		var gSleep = growTime;
+		var hsleep = hackTime;
 		var growSleep = Math.trunc(wSleep) - Math.trunc(gSleep) - 5;
 		var hackSleep = Math.trunc(wSleep) - Math.trunc(hsleep) - 10;
-		var hMaxThreads = Math.ceil(0.70 / ns.hackAnalyze(targetList) / (hostListsS.length - 1));
+		var hMaxThreads = Math.ceil(0.70 / secHa / (hostListsS.length - 1));
 		var hRamThreads = Math.max(Math.trunc((freeRam - wRam) / (wRam + (gRam * ghUpKeep) + hRam)), 0);
 		var hThreads = Math.min(hRamThreads, hMaxThreads);
 		//
-		if (hThreads < 1) { } else if (hThreads >= 1) { await ns.exec(files[0], hostList, hThreads + 1, targetList, 1, crypto.randomUUID().substr(0, 6)) && await ns.exec(files[1], hostList, (hThreads * ghUpKeep), targetList, growSleep, crypto.randomUUID().substr(0, 6)) && await ns.exec(files[2], hostList, hThreads, targetList, hackSleep, crypto.randomUUID().substr(0, 6)) }
+		if (hThreads >= 1) { await _ns("exec", files[0], hostList, hThreads + 1, ddtargetList, 1, crypto.randomUUID().substr(0, 6)) && await _ns("exec", files[1], hostList, (hThreads * ghUpKeep), ddtargetList, growSleep, crypto.randomUUID().substr(0, 6)) && await _ns("exec", files[2], hostList, hThreads, ddtargetList, hackSleep, crypto.randomUUID().substr(0, 6)) }
 	}
 	//
 	if (secNum > 0) {
 		//
-		var wMaxThreads = Math.ceil((ns.getServerSecurityLevel(targetList) - ns.getServerMinSecurityLevel(targetList)) / ns.weakenAnalyze(1, 1) / (hostListsS.length - 1));
+		var wMaxThreads = Math.ceil((secCurrent - secMin) / secWeek / (hostListsS.length - 1));
 		var wRamThreads = Math.max(Math.trunc(freeRam / wRam), 0);
 		var wThreads = Math.min(wRamThreads, wMaxThreads);
 		//
-		if (wThreads < 1) { } else if (wThreads >= 1) { await ns.exec(files[0], hostList, wThreads, targetList, 1) } //, crypto.randomUUID().substr(0, 6)) }
+		if (wThreads >= 1) { await _ns("exec", files[0], hostList, wThreads, ddtargetList, 1) } //, crypto.randomUUID().substr(0, 6)) }
 	}
 	//
 	if (mCash / caSh > 1) {
 		//
-		var gMaxThreads = Math.ceil(ns.growthAnalyze(targetList, ns.getServerMaxMoney(targetList) / Math.max(ns.getServerMoneyAvailable(targetList), 1), 1) / (hostListsS.length - 1));
+		var gMaxThreads = Math.ceil(groA / (hostListsS.length - 1));
 		var gRamThreads = Math.max(Math.floor(freeRam / gRam), 0);
 		var gThreads = Math.min(gRamThreads, gMaxThreads);
 		//
-		if (gThreads < 1) { } else if (gThreads >= 1) { await ns.exec(files[1], hostList, gThreads, targetList, 1) } //, crypto.randomUUID().substr(0, 6)) }
+		if (gThreads >= 1) { await _ns("exec", files[1], hostList, gThreads, ddtargetList, 1) } //, crypto.randomUUID().substr(0, 6)) }
 	}
 }
+export { magicHack };
