cf5fe249a89db883a2a32b8338ac0cc019904142
xaix1999 <88251983+xaix1999@users.noreply.github.com>
2022-04-01 17:26:34 -0700

    Update sCp.js

--- sCp.js
+++ sCp.js
@@ -1,20 +1,22 @@
 /** @param {NS} ns **/
 async function sCp(ns) {
 	var files = ["weak.js", "grow.js", "hack.js", "cryptInit.js"];
-	if (sessionStorage.hostList.split(",") == []) { ns.exit()}
+	if (sessionStorage.hostList.split(",") == []) { ns.exit() }
 	var hostListsS = sessionStorage.hostList.split(",");
 	for (let i = 0; i <= hostListsS.length - 1; i++) {
-		if (!ns.fileExists(files[0], hostListsS[i])) {
-			await ns.scp(files[0], "home", hostListsS[i]);
-		}
-		if (!ns.fileExists(files[1], hostListsS[i])) {
-			await ns.scp(files[1], "home", hostListsS[i]);
-		}
-		if (!ns.fileExists(files[2], hostListsS[i])) {
-			await ns.scp(files[2], "home", hostListsS[i]);
-		}
-		if (!ns.fileExists(files[3], hostListsS[i])) {
-			await ns.scp(files[3], "home", hostListsS[i]);
+		if (ns.serverExists(hostListsS[i])) {
+			if (!ns.fileExists(files[0], hostListsS[i])) {
+				await ns.scp(files[0], "home", hostListsS[i]);
+			}
+			if (!ns.fileExists(files[1], hostListsS[i])) {
+				await ns.scp(files[1], "home", hostListsS[i]);
+			}
+			if (!ns.fileExists(files[2], hostListsS[i])) {
+				await ns.scp(files[2], "home", hostListsS[i]);
+			}
+			if (!ns.fileExists(files[3], hostListsS[i])) {
+				await ns.scp(files[3], "home", hostListsS[i]);
+			}
 		}
 	}
 }
