153d356643f58b387987fe04afd0c781b6ef333a
xaix1999 <88251983+xaix1999@users.noreply.github.com>
2022-04-03 20:44:04 -0700

    Update 8pW.js

--- 8pW.js
+++ 8pW.js
@@ -50,7 +50,7 @@ async function magicHack(ns, cHost, cTarget) {
 	var mCash = Math.trunc(moneyMax * 0.70);
 	var wRam = 1.75; var gRam = 1.75; var hRam = 1.70;
 	//
-	if (secNum < 1 && mCash / caSh <= 1 && freeRam >= 5.20 && growPerHack < 6) {
+	if (secNum < 1 && mCash / caSh <= 1 && freeRam >= 5.20) {
 		//
 		var ghUpKeep = Math.min(growPerHack, 12)
 		var wSleep = weakTime;
